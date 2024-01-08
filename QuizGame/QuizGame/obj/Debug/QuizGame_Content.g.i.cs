412c3dfa06e06187e53513c175c11966bd4f0a90
Laszlo9 <71323987+Laszlo9@users.noreply.github.com>
2024-01-08 19:20:41 +0100

    icon és háttér

--- QuizGame/QuizGame/obj/Debug/QuizGame_Content.g.i.cs
+++ QuizGame/QuizGame/obj/Debug/QuizGame_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("menuhatter.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon_pic.png")]
 
 
